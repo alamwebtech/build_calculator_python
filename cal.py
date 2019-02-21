9d41a1ab7a8f4d67da25211f7bb1bccbc90fc56e
Md Alam <alamwebtech@gmail.com>
2019-02-21 11:31:44 -0500

    adding math operations

--- cal.py
+++ cal.py
@@ -1,7 +1,7 @@
 import math
 
 while True:
-	print("\nChoose the math operation.\n\n0 = Addition\n1 = Subtration\n3 = Multiplication\n4 = Division\n5 = Modulo\n6 Raising to a power\n7 = Square root\n8 = Sine\n9 = Cosine\n10 = Tangent\n")
+	print("\nChoose the math operation.\n\n0 = Addition\n1 = Subtration\n3 = Multiplication\n4 = Division\n5 = Modulo\n6 = Raising to a power\n7 = Square root\n8 = Sine\n9 = Cosine\n10 = Tangent\n")
 
 oper = input("\nYour option from the menu:")
 
@@ -75,8 +75,72 @@ elif oper == "4":
 	else:
 		break 
 
+# Raising to a power
 
+elif oper == "5":
+	val1 = float(input("\nFirst value: "))
+	val2 = float(input("\nSecond value: "))
 
+	print("\nThe result is: " + str(math.pow(val1,val2)) + "\n")
 
+	back = input("\nGo back to the main menu ? (y/n)")
+	if back == "y":
+		continue
+	else:
+		break 
 
+# Square root
 
+elif oper == "6":
+	val1 = float(input("\nEnter value for extracting the square root: "))
+	
+
+	print("\nThe result is: " + str(math.sqrt(val1)) + "\n")
+
+	back = input("\nGo back to the main menu ? (y/n)")
+	if back == "y":
+		continue
+	else:
+		break 
+
+# Logarithm
+
+elif oper == "7":
+	val1 = float(input("\nEnter the value for calculating the logarithm: "))
+	
+
+	print("\nThe result is: " + str(math.log(val1, 2)) + "\n")
+
+	back = input("\nGo back to the main menu ? (y/n)")
+	if back == "y":
+		continue
+	else:
+		break 
+
+# Sine
+
+elif oper == "8":
+	val1 = float(input("\nEnter the value (in degrees) for calculating the sine: "))
+	
+
+	print("\nThe result is: " + str(math.sin(math.radians(val1)))+ "\n")
+
+	back = input("\nGo back to the main menu ? (y/n)")
+	if back == "y":
+		continue
+	else:
+		break 
+
+# Cosine
+
+elif oper == "9":
+	val1 = float(input("\nEnter the value (in degrees) for calculating the sine: "))
+	
+
+	print("\nThe result is: " + str(math.cos(math.radians(val1)))+ "\n")
+
+	back = input("\nGo back to the main menu ? (y/n)")
+	if back == "y":
+		continue
+	else:
+		break
